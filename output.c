c6413b74fe6be7930773d666672ca1af78316c68
Gabriel de Biasi <biasi131@gmail.com>
2015-09-23 13:48:41 -0400

    adicionando o out()

---
+++ output.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define RESTO(x,y) (x%y)
+#define ABS(x) (x >= 0 ? x:-x)
+#define TRUNCA(x) ((int) (x/1))
+
+int main (int argc, char* argv[]) {
+if () {
+RESTOif () {
+} else {
+}
+}
+return 0;
+}
